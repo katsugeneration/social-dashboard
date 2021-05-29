beb369ebaf817475bb883434c0ef5c09d0252046
Katsuya Shimabukuro <katsu.generation.888@gmail.com>
2021-05-29 13:44:42 +0000

    Update sql for new formater

--- dbt/models/component/jasso_gakuseiseikatsu_stats_annual_income_divide_university_type_and_income.sql
+++ dbt/models/component/jasso_gakuseiseikatsu_stats_annual_income_divide_university_type_and_income.sql
@@ -1,37 +1,49 @@
--- {{ config(materialized = 'view') }} 
+{{ config(
+    materialized = 'view'
+) }}
+
 WITH temp_incomes AS (
-    SELECT university_type,
-        year,
+
+    SELECT
+        university_type,
+        YEAR,
         CASE
             `range`
             WHEN '200万円未満' THEN 150
             ELSE CAST(
-                REPLACE(REGEXP_EXTRACT(`range`, "[0-9,]*"), ',', '') AS INT64
+                REPLACE(regexp_extract(`range`, "[0-9,]*"), ',', '') AS int64
             ) + 50
         END AS range_value,
         `range`,
         rate
-    FROM social_dataset.jasso_gakuseiseikatsu_stats_annual_income_divide_university
-    WHERE sex = '平均'
+    FROM
+        social_dataset.jasso_gakuseiseikatsu_stats_annual_income_divide_university
+    WHERE
+        sex = '平均'
 ),
 cum_rates AS (
-    SELECT university_type,
-        year,
+    SELECT
+        university_type,
+        YEAR,
         `range`,
         range_value,
         rate,
-        sum(rate) OVER (
+        SUM(rate) over (
             PARTITION BY university_type,
-            year
-            ORDER BY range_value
-        ) as cumrate
-    FROM temp_incomes
-    ORDER BY university_type,
+            YEAR
+            ORDER BY
+                range_value
+        ) AS cumrate
+    FROM
+        temp_incomes
+    ORDER BY
+        university_type,
         range_value
 ),
 divide_1st AS (
-    SELECT university_type,
-        year,
+    SELECT
+        university_type,
+        YEAR,
         SUM(
             range_value * IF(
                 cumrate <= 20,
@@ -43,17 +55,20 @@ divide_1st AS (
                 )
             )
         ) / 20 AS avg_income
-    FROM cum_rates
-    GROUP BY university_type,
-        year
+    FROM
+        cum_rates
+    GROUP BY
+        university_type,
+        YEAR
 ),
 divide_2nd AS (
-    SELECT university_type,
-        year,
+    SELECT
+        university_type,
+        YEAR,
         SUM(
             range_value * IF(
                 20 < cumrate - rate
-                and cumrate <= 40,
+                AND cumrate <= 40,
                 rate,
                 IF(
                     cumrate <= 20
@@ -67,17 +82,20 @@ divide_2nd AS (
                 )
             )
         ) / 20 AS avg_income
-    FROM cum_rates
-    GROUP BY university_type,
-        year
+    FROM
+        cum_rates
+    GROUP BY
+        university_type,
+        YEAR
 ),
 divide_3rd AS (
-    SELECT university_type,
-        year,
+    SELECT
+        university_type,
+        YEAR,
         SUM(
             range_value * IF(
                 40 < cumrate - rate
-                and cumrate <= 60,
+                AND cumrate <= 60,
                 rate,
                 IF(
                     cumrate <= 40
@@ -91,17 +109,20 @@ divide_3rd AS (
                 )
             )
         ) / 20 AS avg_income
-    FROM cum_rates
-    GROUP BY university_type,
-        year
+    FROM
+        cum_rates
+    GROUP BY
+        university_type,
+        YEAR
 ),
 divide_4th AS (
-    SELECT university_type,
-        year,
+    SELECT
+        university_type,
+        YEAR,
         SUM(
             range_value * IF(
                 60 < cumrate - rate
-                and cumrate <= 80,
+                AND cumrate <= 80,
                 rate,
                 IF(
                     cumrate <= 60
@@ -115,13 +136,16 @@ divide_4th AS (
                 )
             )
         ) / 20 AS avg_income
-    FROM cum_rates
-    GROUP BY university_type,
-        year
+    FROM
+        cum_rates
+    GROUP BY
+        university_type,
+        YEAR
 ),
 divide_5th AS (
-    SELECT university_type,
-        year,
+    SELECT
+        university_type,
+        YEAR,
         SUM(
             range_value * IF(
                 80 < cumrate - rate,
@@ -134,23 +158,31 @@ divide_5th AS (
                 )
             )
         ) / 20 AS avg_income
-    FROM cum_rates
-    GROUP BY university_type,
-        year
+    FROM
+        cum_rates
+    GROUP BY
+        university_type,
+        YEAR
 )
-SELECT divide_1st.university_type,
+SELECT
+    divide_1st.university_type,
     divide_1st.year,
     divide_1st.avg_income AS divide_1st,
     divide_2nd.avg_income AS divide_2nd,
     divide_3rd.avg_income AS divide_3rd,
     divide_4th.avg_income AS divide_4th,
     divide_5th.avg_income AS divide_5th
-FROM divide_1st
-    INNER JOIN divide_2nd ON divide_1st.university_type = divide_2nd.university_type
+FROM
+    divide_1st
+    INNER JOIN divide_2nd
+    ON divide_1st.university_type = divide_2nd.university_type
     AND divide_1st.year = divide_2nd.year
-    INNER JOIN divide_3rd ON divide_1st.university_type = divide_3rd.university_type
+    INNER JOIN divide_3rd
+    ON divide_1st.university_type = divide_3rd.university_type
     AND divide_1st.year = divide_3rd.year
-    INNER JOIN divide_4th ON divide_1st.university_type = divide_4th.university_type
+    INNER JOIN divide_4th
+    ON divide_1st.university_type = divide_4th.university_type
     AND divide_1st.year = divide_4th.year
-    INNER JOIN divide_5th ON divide_1st.university_type = divide_5th.university_type
+    INNER JOIN divide_5th
+    ON divide_1st.university_type = divide_5th.university_type
     AND divide_1st.year = divide_5th.year
