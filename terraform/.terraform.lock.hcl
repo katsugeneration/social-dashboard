722628e695d0c2f9c18c18092ff4c81396af4aa0
Katsuya Shimabukuro <katsu.generation.888@gmail.com>
2021-06-06 16:29:34 +0900

    Add data catalog tag template

--- terraform/.terraform.lock.hcl
+++ terraform/.terraform.lock.hcl
@@ -2,20 +2,39 @@
 # Manual edits may be lost in future updates.
 
 provider "registry.terraform.io/hashicorp/aws" {
-  version     = "3.40.0"
+  version     = "3.44.0"
   constraints = "~> 3.0"
   hashes = [
-    "h1:ks2Xxdv9uu4oKbBgZssqlyproDaxeQ1dR+/CrtNwDS0=",
-    "zh:2fd824991b19837e200d19b17d8188bf71efb92c36511809484549e77b4045dd",
-    "zh:47250cb58b3bd6f2698ca17bfb962710542d6adf95637cd560f6119abf97dba2",
-    "zh:515722a8c8726541b05362ec71331264977603374a2e4d4d64f89940873143ea",
-    "zh:61b6b7542da2113278c987a0af9f230321f5ed605f1e3098824603cb09ac771b",
-    "zh:66aad13ada6344b64adbc67abad4f35c414e62838a99f78626befb8b74c760d8",
-    "zh:7d4436aeb53fa348d7fd3c2ab4a727b03c7c59bfdcdecef4a75237760f3bb3cf",
-    "zh:a4583891debc49678491510574b1c28bb4fe3f83ed2bb353959c4c1f6f409f1f",
-    "zh:b8badecea52f6996ae832144560be87e0b7c2da7fe1dcd6e6230969234b2fc55",
-    "zh:cecf64a085f640c30437ccc31bd964c21004ae8ae00cfbd95fb04037e46b88ca",
-    "zh:d81dbb9ad8ce5eca4d1fc5a7a06bbb9c47ea8691f1502e94760fa680e20e4afc",
-    "zh:f0fc724a964c7f8154bc5911d572ee411f5d181414f9b1f09de7ebdacb0d884b",
+    "h1:hxQ8n9SHHfAIXd/FtfAqxokFYWBedzZf7xqQZWJajUs=",
+    "zh:0680315b29a140e9b7e4f5aeed3f2445abdfab31fc9237f34dcad06de4f410df",
+    "zh:13811322a205fb4a0ee617f0ae51ec94176befdf569235d0c7064db911f0acc7",
+    "zh:25e427a1cfcb1d411bc12040cf0684158d094416ecf18889a41196bacc761729",
+    "zh:40cd6acd24b060823f8d116355d8f844461a11925796b1757eb2ee18abc0bc7c",
+    "zh:94e2463eef555c388cd27f6e85ad803692d6d80ffa621bdc382ab119001d4de4",
+    "zh:aadc3bc216b14839e85b463f07b8507920ace5f202a608e4a835df23711c8a0d",
+    "zh:ab50dc1242af5a8fcdb18cf89beeaf2b2146b51ecfcecdbea033913a5f4c1c14",
+    "zh:ad48bbf4af66b5d48ca07c5c558d2f5724311db4dd943c1c98a7f3f107e03311",
+    "zh:ad76796c2145a7aaec1970a5244f5c0a9d200556121e2c5b382f296597b1a03c",
+    "zh:cf0a2181356598f8a2abfeaf0cdf385bdeea7f2e52821c850a2a08b60c26b9f6",
+    "zh:f76801af6bc34fe4a5bf1c63fa0204e24b81691049efecd6baa1526593f03935",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/google" {
+  version     = "3.70.0"
+  constraints = "~> 3.0"
+  hashes = [
+    "h1:fj//smNY6tC7/Zy8G/YeFkFTKgXkOIG99/FMeTti4Ik=",
+    "zh:26f37fc308ddb20baf20efd93726b2ff7894310c4980c07f3e0ae467ff3cad82",
+    "zh:40cd363b7a325833685940b5fd6d6ba5a54d1a637dba06ae05114facdf7f49a9",
+    "zh:42ee807cba7f0e1c06b52b3a70ee5da707a38d7a73a459e99cadd733a38f53a5",
+    "zh:57d6fd677c699be7ae97cfcd831283e2d04b1e168c9906ab49a499663ba0c801",
+    "zh:7238128698516b9a6f7d49b1f772aeee0234e162997ca5fd16315c6a57c8fead",
+    "zh:77d923faac5dd9744a4e0ba4d47a8b2de19358fff9b2060b82b127694a48c9d2",
+    "zh:7fdfb1b0bce09bbae8ab4d6c44d72ddbaddddf14c6aca3d952f71e03a57d9d0d",
+    "zh:a14af8edad375b15502cb33c2ac9a401b14c891832b4257056d86a4f65a453f7",
+    "zh:c8a7b2202db3ffaad11011911181a382f3b55a0804d3c0a1177e6431a391e426",
+    "zh:cd0818982ee24c8bd1caed93816b6f15fa1cef07de39d1edf5110fd17e892430",
+    "zh:d2abded6c1088a85d7487369998c71652a338b46b1646e67676a717ff1f394f8",
   ]
 }
---
+++ terraform/catalog.tf
@@ -0,0 +1,186 @@
+resource "google_data_catalog_tag_template" "data_ingestion" {
+  tag_template_id = "data_ingestion"
+  display_name    = "Data Ingestion"
+
+  fields {
+    display_name = "License"
+    description  = "Data License"
+    field_id     = "license"
+    is_required  = true
+    order        = 0
+
+    type {
+      primitive_type = "STRING"
+    }
+  }
+  fields {
+    display_name = "Last ETL Job End"
+    description  = "End-time of most recent ingestion job"
+    field_id     = "latest_job_end_datetime"
+    is_required  = false
+    order        = 10
+
+    type {
+      primitive_type = "TIMESTAMP"
+    }
+  }
+  fields {
+    display_name = "Last ETL Job Runtime"
+    description  = "How long the most recent ETL job was running"
+    field_id     = "latest_job_run_time"
+    is_required  = false
+    order        = 9
+
+    type {
+      primitive_type = "STRING"
+    }
+  }
+  fields {
+    display_name = "Data Ingestion Owner"
+    description  = "Name of the user to contact regarding ingestion issues and questions"
+    field_id     = "data_ingestion_owner"
+    is_required  = false
+    order        = 13
+
+    type {
+      primitive_type = "STRING"
+    }
+  }
+  fields {
+    display_name = "Number of Errors"
+    description  = "Specifies how many errors have occurred during most recent ETL job"
+    field_id     = "errors"
+    is_required  = false
+    order        = 1
+
+    type {
+      primitive_type = "DOUBLE"
+    }
+  }
+  fields {
+    display_name = "Number of Warnings"
+    description  = "Specifies how many warnings have occurred during most recent ETL job"
+    field_id     = "warnings"
+    is_required  = false
+    order        = 2
+
+    type {
+      primitive_type = "DOUBLE"
+    }
+  }
+  fields {
+    display_name = "Data Sources of ETL Job"
+    description  = "Specifies the source of the data asset"
+    field_id     = "data_sources"
+    is_required  = false
+    order        = 8
+
+    type {
+      primitive_type = "STRING"
+    }
+  }
+  fields {
+    display_name = "PII Data Sources"
+    description  = "Specifies whether all source data contains PII"
+    field_id     = "data_sources_have_pii"
+    is_required  = false
+    order        = 6
+
+    type {
+      primitive_type = "BOOL"
+    }
+  }
+  fields {
+    display_name = "Data Sources Approved"
+    description  = "Specifies whether all source data was approved"
+    field_id     = "data_sources_approved_all"
+    is_required  = false
+    order        = 7
+
+    type {
+      primitive_type = "BOOL"
+    }
+  }
+  fields {
+    display_name = "Rows to Reprocess"
+    description  = "The number of rows that need reprocessing"
+    field_id     = "rows_for_reprocessing"
+    is_required  = false
+    order        = 3
+
+    type {
+      primitive_type = "DOUBLE"
+    }
+  }
+  fields {
+
+    display_name = "Rows Added"
+    description  = "The number of rows/records added during the most recent ETL job run"
+    field_id     = "rows_added"
+    is_required  = false
+    order        = 4
+
+    type {
+      primitive_type = "DOUBLE"
+    }
+  }
+  fields {
+    display_name = "Rows Processed"
+    description  = "The number of rows/records ingested/processed during the most recent ETL job run"
+    field_id     = "rows_processed"
+    is_required  = false
+    order        = 5
+
+    type {
+      primitive_type = "DOUBLE"
+    }
+  }
+  fields {
+    display_name = "Last ETL Job Start"
+    description  = "The start-time of most recent ETL job"
+    field_id     = "latest_job_start_datetime"
+    is_required  = false
+    order        = 11
+
+    type {
+      primitive_type = "TIMESTAMP"
+    }
+  }
+  fields {
+    description  = "The status of the last ingestion job"
+    display_name = "Last ETL Job Status"
+    field_id     = "latest_job_status"
+    is_required  = false
+    order        = 12
+
+    type {
+      enum_type {
+        allowed_values {
+          display_name = "running"
+        }
+        allowed_values {
+          display_name = "failed"
+        }
+        allowed_values {
+          display_name = "interrupted"
+        }
+        allowed_values {
+          display_name = "partial"
+        }
+        allowed_values {
+          display_name = "queued"
+        }
+        allowed_values {
+          display_name = "retrying"
+        }
+        allowed_values {
+          display_name = "running"
+        }
+        allowed_values {
+          display_name = "skipped"
+        }
+      }
+    }
+  }
+  timeouts {}
+}
