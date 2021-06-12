9ed476b7acb5c7ff8f4b89ab6b698f2e2ab74110
Katsuya Shimabukuro <katsu.generation.888@gmail.com>
2021-06-12 06:14:57 +0000

    rename to e-stat-importer

--- terraform/trigger.tf
+++ terraform/trigger.tf
@@ -61,3 +61,62 @@ resource "google_pubsub_subscription" "jasso_gakuseiseikatsu_stats_importer_dl" 
 
   timeouts {}
 }
+
+resource "google_pubsub_topic" "e_stat_importer" {
+  name = "e-stat-importer"
+}
+
+resource "google_pubsub_topic" "e_stat_importer_dl" {
+  name = "e-stat-importer-dl"
+}
+
+resource "google_pubsub_subscription" "e_stat_importer" {
+  ack_deadline_seconds       = 10
+  enable_message_ordering    = false
+  labels                     = {}
+  message_retention_duration = "604800s"
+  name                       = "e-stat-importer"
+  retain_acked_messages      = false
+  topic                      = google_pubsub_topic.e_stat_importer.id
+
+  expiration_policy {
+    ttl = "2678400s"
+  }
+
+  push_config {
+    attributes    = {}
+    push_endpoint = module.runner["e-stat-importer"].url
+
+    oidc_token {
+      service_account_email = google_service_account.pubsub_social_dashboard.email
+    }
+  }
+
+  dead_letter_policy {
+    max_delivery_attempts = 5
+    dead_letter_topic     = google_pubsub_topic.e_stat_importer_dl.id
+  }
+
+  retry_policy {
+    maximum_backoff = "600s"
+    minimum_backoff = "10s"
+  }
+
+  timeouts {}
+}
+
+resource "google_pubsub_subscription" "e_stat_importer_dl" {
+  ack_deadline_seconds       = 10
+  enable_message_ordering    = false
+  labels                     = {}
+  message_retention_duration = "604800s"
+  name                       = "e-stat-importer-dl"
+  retain_acked_messages      = false
+  topic                      = google_pubsub_topic.e_stat_importer_dl.id
+
+  expiration_policy {
+    ttl = "2678400s"
+  }
+
+  timeouts {}
+}
