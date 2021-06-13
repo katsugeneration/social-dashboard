c48d1c3eedf5f71f46c3ce38aad50e8a03e96005
Katsuya Shimabukuro <katsu.generation.888@gmail.com>
2021-06-13 11:45:48 +0900

    Add annual_income_divide_5_quintile view

---
+++ dbt/models/model/annual_income_divide_5_quintile.sql
@@ -0,0 +1,25 @@
+{{ config(
+    materialized = 'view'
+) }}
+
+SELECT
+    CONCAT(
+        "大学: ",
+        university_type
+    ) AS TYPE,
+    YEAR,
+    quintile,
+    VALUE * 1000 AS VALUE
+FROM
+    social_dataset.jasso_gakuseiseikatsu_stats_annual_income_divide_university_type_and_income
+UNION ALL
+SELECT
+    houseshold_type AS TYPE,
+    YEAR,
+    quintile,
+    VALUE * 10 AS VALUE
+FROM
+    social_dataset.ja_kakei_chousa_income_divide_over_two_member
+WHERE
+    acquisition_year = 2020
+    AND aggregation_type = '実収入'
